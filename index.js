ae7fca3c379a07fc398d425a7c9053f13709a0d6
John Leider <john.j.leider@gmail.com>
2018-12-03 14:09:37 -0600

    feat: update rules to new defaults

--- index.js
+++ index.js
@@ -12,31 +12,25 @@ module.exports = {
 
   rules: {
     'max-len': 'off',
+    'indent': 'off',
+    'vue/component-name-in-template-casing': ['error', 'kebab-case'],
+    'vue/script-indent': ['error', 2, {
+      'baseIndent': 1,
+      'switchCase': 1,
+      'ignores': []
+    }],
     'vue/max-attributes-per-line': ['error', {
-      'singleline': 5,
+      'singleline': 1,
       'multiline': {
         'max': 1,
         'allowFirstLine': false
       }
-    }]
-  },
-
-  overrides: [
-    {
-      files: '**/*.vue',
-      rules: {
-        indent: 'off',
-        'vue/script-indent': ['error', 2, {
-          'baseIndent': 1,
-          'switchCase': 1,
-          'ignores': []
-        }],
-        'vue/html-closing-bracket-newline': ['error', {
-          'singleline': 'never',
-          'multiline': 'always'
-        }],
-        'vue/html-closing-bracket-spacing': 'error'
-      }
-    }
-  ]
+    }],
+    'vue/html-closing-bracket-newline': ['error', {
+      'singleline': 'never',
+      'multiline': 'always'
+    }],
+    'vue/html-closing-bracket-spacing': 'error',
+    'vue/no-v-html': 'off'
+  }
 }
