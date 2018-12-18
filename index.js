0eb758dc463ce974009a6f0746765f8d1249bf01
John Leider <john.j.leider@gmail.com>
2018-12-18 13:15:59 -0500

    chore: move vue rules into file type scoped overrides

--- index.js
+++ index.js
@@ -3,34 +3,39 @@ module.exports = {
     'standard',
     'plugin:vue/recommended'
   ],
-
   parserOptions: {
     parser: 'babel-eslint',
     ecmaVersion: 2017,
     sourceType: 'module'
   },
-
   rules: {
     'max-len': 'off',
-    'indent': 'off',
-    'vue/component-name-in-template-casing': ['error', 'kebab-case'],
-    'vue/script-indent': ['error', 2, {
-      'baseIndent': 1,
-      'switchCase': 1,
-      'ignores': []
-    }],
-    'vue/max-attributes-per-line': ['error', {
-      'singleline': 1,
-      'multiline': {
-        'max': 1,
-        'allowFirstLine': false
+    'indent': 'off'
+  },
+  overrides: [
+    {
+      files: '**/*.vue"',
+      rules: {
+        'vue/component-name-in-template-casing': ['error', 'kebab-case'],
+        'vue/script-indent': ['error', 2, {
+          'baseIndent': 1,
+          'switchCase': 1,
+          'ignores': []
+        }],
+        'vue/max-attributes-per-line': ['error', {
+          'singleline': 1,
+          'multiline': {
+            'max': 1,
+            'allowFirstLine': false
+          }
+        }],
+        'vue/html-closing-bracket-newline': ['error', {
+          'singleline': 'never',
+          'multiline': 'always'
+        }],
+        'vue/html-closing-bracket-spacing': 'error',
+        'vue/no-v-html': 'off'
       }
-    }],
-    'vue/html-closing-bracket-newline': ['error', {
-      'singleline': 'never',
-      'multiline': 'always'
-    }],
-    'vue/html-closing-bracket-spacing': 'error',
-    'vue/no-v-html': 'off'
-  }
+    }
+  ]
 }
