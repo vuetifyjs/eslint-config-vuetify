f55aa834c2a8949a7d68332d9ed1cde6342a90f4
WebDevNerdStuff <webdevnerdstuff@gmail.com>
2024-11-05 18:29:16 -0800

    updting for eslint 9

--- index.cjs
+++ index.cjs
@@ -1,71 +1,104 @@
+import pluginVue from 'eslint-plugin-vue';
+import globals from 'globals';
+import vueTsEslintConfig from '@vue/eslint-config-typescript';
+
+export default [
+  ...pluginVue.configs['flat/essential'],
+  ...vueTsEslintConfig(),
+  {
+		name: 'app/files-to-lint',
+    files: '**/*.vue',
+    languageOptions: {
+			globals: {
+				...globals.node,
+			},
+		},
+    rules: {
+      'no-var': 'error',
+      // allow paren-less arrow functions
+      'arrow-parens': ['error', 'as-needed'],
+      complexity: ['error', 32],
+      quotes: ['error', 'single', {
+        avoidEscape: true,
+        allowTemplateLiterals: true,
+      }],
+      'no-console': 'off',
+      'comma-dangle': ['error', {
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        imports: 'always-multiline',
+        exports: 'always-multiline',
+        functions: 'only-multiline',
+      }],
+      // allow debugger during development
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+      'no-return-assign': 'off',
+      'no-unused-vars': 'error',
+      'no-empty': 'error',
+      'array-bracket-spacing': ['error', 'never'],
+      'object-curly-spacing': ['error', 'always'],
+      'space-before-function-paren': [
+        'error',
+        {
+          anonymous: 'always',
+          named: 'always',
+          asyncArrow: 'always',
+        },
+      ],
+      'no-return-await': 'warn',
+      'object-shorthand': ['error', 'always'],
+      'no-extra-semi': 'error',
+      'prefer-const': ['error', {
+        destructuring: 'all',
+        ignoreReadBeforeAssign: true,
+      }],
+      'no-prototype-builtins': 'off',
+      'no-void': 'off',
+      'no-case-declarations': 'off',
+      'sort-imports': ['warn', {
+        ignoreDeclarationSort: true,
+        ignoreCase: true,
+      }],
+      'multiline-ternary': 'off',
+
+      // Not in override, these apply to non-.vue files too
+      'vue/require-default-prop': 'off',
+      'vue/require-prop-types': 'off',
+      'vue/one-component-per-file': 'off',
+      'vue/custom-event-name-casing': ['error', { ignores: ['/^[a-z]+(?:-[a-z]+)*:[a-z]+(?:-[a-z]+)*$/u'] }],
+
+      'vue/attributes-order': ['error', {
+        alphabetical: true,
+      }],
+
+      // old overrides - ref https://eslint.org/docs/latest/use/migrate-to-9.0.0#--eslint--comments-with-only-severity-now-retain-options-from-the-config-file
+      indent: 'off',
+      'vue/script-indent': ['error', 2, {
+        baseIndent: 1,
+        switchCase: 1,
+        ignores: [],
+      }],
+      'vue/html-closing-bracket-newline': ['error', {
+        singleline: 'never',
+        multiline: 'always',
+      }],
+      'vue/html-closing-bracket-spacing': 'error',
+      'vue/max-attributes-per-line': ['error', {
+        singleline: 4,
+        multiline: 1,
+      }],
+      'vue/valid-v-on': 'off', // This rule doesn't allow empty event listeners
+      'vue/no-v-html': 'off',
+      'vue/singleline-html-element-content-newline': 'off',
+      'vue/multiline-html-element-content-newline': 'off',
+      'vue/valid-v-slot': ['error', { allowModifiers: true }],
+      'vue/multi-word-component-names': 'off',
+    }
+  }
+]
+
+
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    'plugin:vue/vue3-recommended',
-    'standard',
-    '@vue/eslint-config-typescript',
-  ],
-  rules: {
-    'no-var': 'error',
-    // allow paren-less arrow functions
-    'arrow-parens': ['error', 'as-needed'],
-    complexity: ['error', 32],
-    quotes: ['error', 'single', {
-      avoidEscape: true,
-      allowTemplateLiterals: true,
-    }],
-    'no-console': 'off',
-    'comma-dangle': ['error', {
-      arrays: 'always-multiline',
-      objects: 'always-multiline',
-      imports: 'always-multiline',
-      exports: 'always-multiline',
-      functions: 'only-multiline',
-    }],
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-return-assign': 'off',
-    'no-unused-vars': 'error',
-    'no-empty': 'error',
-    'array-bracket-spacing': ['error', 'never'],
-    'object-curly-spacing': ['error', 'always'],
-    'space-before-function-paren': [
-      'error',
-      {
-        anonymous: 'always',
-        named: 'always',
-        asyncArrow: 'always',
-      },
-    ],
-    'no-return-await': 'warn',
-    'object-shorthand': ['error', 'always'],
-    'no-extra-semi': 'error',
-    'prefer-const': ['error', {
-      destructuring: 'all',
-      ignoreReadBeforeAssign: true,
-    }],
-    'no-prototype-builtins': 'off',
-    'no-void': 'off',
-    'no-case-declarations': 'off',
-    'sort-imports': ['warn', {
-      ignoreDeclarationSort: true,
-      ignoreCase: true,
-    }],
-    'multiline-ternary': 'off',
-
-    // Not in override, these apply to non-.vue files too
-    'vue/require-default-prop': 'off',
-    'vue/require-prop-types': 'off',
-    'vue/one-component-per-file': 'off',
-    'vue/custom-event-name-casing': ['error', { ignores: ['/^[a-z]+(?:-[a-z]+)*:[a-z]+(?:-[a-z]+)*$/u'] }],
-
-    'vue/attributes-order': ['error', {
-      alphabetical: true,
-    }],
-  },
   overrides: [
     {
       files: '**/*.vue',
