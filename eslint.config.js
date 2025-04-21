ae1c8334c28c65bd424b9e01743b14a86c40599f
Andrei Elkin <andreyyolkin@gmail.com>
2025-04-21 21:00:34 +0200

    chore: use default config inside the repo

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,3 @@
 import withConfig from 'eslint-config-vuetify'
 
-export default withConfig(
-  {
-    files: ['**/*.js'],
-    rules: {
-      '@stylistic/comma-dangle': ['error', 'never'],
-      'import/default': 'off'
-    }
-  })
+export default withConfig()
